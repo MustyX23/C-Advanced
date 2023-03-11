3e9944bfe9f8ae675a954898a86d45bfb5a5de5f
MustyX23 <mustyx900@gmail.com>
2023-03-11 10:35:19 +0200

    3. Simple Calculator

---
+++ Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ").Reverse().ToArray();
+            Stack<string> stack = new Stack<string>(input);
+
+            while (stack.Count > 1)
+            {
+                int firstNumber = int.Parse(stack.Pop());
+                string symbol = stack.Pop();
+                int secondNumber = int.Parse(stack.Pop());
+
+                if (symbol == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+                else if (symbol == "-")
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+
+            }
+
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
