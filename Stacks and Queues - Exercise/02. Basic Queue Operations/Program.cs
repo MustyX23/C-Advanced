0671007072a7bf633656bb6ceb67709a9dee46de
MustyX23 <mustyx900@gmail.com>
2023-03-13 21:19:19 +0200

    02. Basic Queue Operations

---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int N = array[0];
+            int S = array[1];
+            int X = array[2];
+
+            int[] workPlace = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(workPlace);
+
+            for (int i = 0; i < S; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(X))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                int smallestNumber = queue.AsQueryable().Min();
+                Console.WriteLine(smallestNumber);
+            }
+        }
+    }
+}
