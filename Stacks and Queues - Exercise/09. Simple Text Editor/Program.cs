e795b8cc0508532f0b0655a0ca6a2c2a092a2749
MustyX23 <mustyx900@gmail.com>
2023-03-14 20:15:25 +0200

    09. Simple Text Editor

---
+++ Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string text = string.Empty;
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                if (command[0] == "1")
+                {
+                    stack.Push(text);
+                    text += command[1];
+
+                }
+                else if (command[0] == "2")
+                {
+                    stack.Push(text);
+                    int count = int.Parse(command[1]);
+                    text = text.Substring(0, text.Length - count);
+                }
+                else if (command[0] == "3")
+                {
+                    int index = int.Parse(command[1]);
+                    Console.WriteLine(text[index - 1]);
+
+                }
+                else if (command[0] == "4")
+                {
+                    text = stack.Pop();
+                }
+            }
+        }
+    }
+}
