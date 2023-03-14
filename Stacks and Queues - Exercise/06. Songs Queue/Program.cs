d8d441a3cd3c907296c504d654f52508a9312a82
MustyX23 <mustyx900@gmail.com>
2023-03-14 20:14:48 +0200

    06. Songs Queue

---
+++ Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the initial sequence of songs
+            string[] songs = Console.ReadLine().Split(", ");
+
+            // Create a queue to hold the songs
+            Queue<string> queue = new Queue<string>(songs);
+
+            // Process commands until the queue is empty
+            while (queue.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    // Remove the first song from the queue
+                    string song = queue.Dequeue();
+                }
+                else if (command.StartsWith("Add "))
+                {
+                    string song = command.Substring(4);
+
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+                }
+                else if (command == "Show")
+                {
+                    // Print all songs in the queue
+                    Console.WriteLine(string.Join(", ", queue));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
