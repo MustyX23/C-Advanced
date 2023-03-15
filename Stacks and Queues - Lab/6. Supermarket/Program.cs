beb54827fc9f49ae498b25d9ec64eb60b40d41ea
MustyX23 <mustyx900@gmail.com>
2023-03-15 21:05:08 +0200

    6. Supermarket

---
+++ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            string input;
+            while ((input = Console.ReadLine())!= "End")
+            {
+                if (input == "Paid")
+                {
+                    int savedQueue = queue.Count;
+                    for (int i = 0; i < savedQueue; i++)
+                    {
+                        Console.WriteLine(queue.Dequeue()); 
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+                
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
