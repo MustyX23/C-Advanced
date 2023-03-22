19a49b32703cc5cc9d0dc8113dd58a28839cd28e
MustyX23 <mustyx900@gmail.com>
2023-03-22 22:22:51 +0200

    03. Primary Diagonal

---
+++ Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[square, square];
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[]elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = elements[column];
+                }
+            }
+
+            for (int i = 0; i < square; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
