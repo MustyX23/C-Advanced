deaf673fb4802cd5fa7db8a55b8400dfbfccbd9f
MustyX23 <mustyx900@gmail.com>
2023-04-01 20:35:25 +0300

    01. Diagonal Difference

---
+++ Multidimensional Arrays - Exercise/01. Diagonal Difference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int [n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {                    
+                    matrix[row, col] = elements[col];
+                }
+            }
+
+            int leftSum = 0;
+            int rightSum = 0;
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                leftSum += matrix[i, i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                rightSum += matrix[i, n - i - 1];
+            }
+
+            int sum = Math.Abs(leftSum - rightSum);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
