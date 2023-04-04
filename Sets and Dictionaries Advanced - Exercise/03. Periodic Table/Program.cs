6d6343a3b2069a88a98111e9653f779517ddd7ff
MustyX23 <mustyx900@gmail.com>
2023-04-05 00:35:33 +0300

    03. Periodic Table\Program.cs

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> chemicalCompounds = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemicalCompound = Console.ReadLine().Split(' ');
+                for (int j = 0; j < chemicalCompound.Length; j++)
+                {
+                    chemicalCompounds.Add(chemicalCompound[j]);
+                }
+                
+            }
+
+            foreach (var chemicalCompound in chemicalCompounds.OrderBy(x => x))
+            {
+                Console.Write($"{chemicalCompound} ");
+            }
+        }
+    }
+}
