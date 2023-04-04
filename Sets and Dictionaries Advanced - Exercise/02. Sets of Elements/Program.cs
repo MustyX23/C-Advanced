65c9cb97b48b425e4b2256d3771ade76566631f6
MustyX23 <mustyx900@gmail.com>
2023-04-05 00:35:15 +0300

    02. Sets of Elements

---
+++ Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> integers = new HashSet<int>();
+            HashSet<int> result = new HashSet<int>();
+
+            int[] size = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int n = size[0];
+            int m = size[1];
+
+            for (int i = 0; i < n + m; i++)
+            {
+                int ints = int.Parse(Console.ReadLine());
+                if (i < n)
+                {
+                    integers.Add(ints);
+                }
+                else
+                {
+                    result.Add(ints);
+                }              
+            }
+
+            integers.IntersectWith(result);
+            
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
