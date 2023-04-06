64a8aa7d1258d5cbbc7cf9c3976cfab1f3fdfbf3
MustyX23 <mustyx900@gmail.com>
2023-04-06 21:27:21 +0300

    02. Blind Man's Buff.docx

---
+++ C# Advanced Exam - 18 February 2023/02. Blind Man's Buff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Blind_Man_s_Buff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
