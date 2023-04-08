2960a0a12a7f0c26cf6d7dcf8eb93b693a03f103
MustyX23 <mustyx900@gmail.com>
2023-04-09 00:40:19 +0300

    03. Largest 3 Numbers\Program.cs

---
+++ Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] sortedNums = nums.OrderByDescending(x => x).Take(3).ToArray();
+
+            for (int i = 0; i < sortedNums.Length; i++)
+            {
+                Console.Write(sortedNums[i] + " ");
+            }
+        }
+    }
+}
