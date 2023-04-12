ed875c78812710c7fc200aa8fbab02c066ab5e04
MustyX23 <mustyx900@gmail.com>
2023-04-12 21:13:59 +0300

    07. Parking Lot\Program.cs

---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] commands = input.Split(", ");
+
+                string direction = commands[0];
+                string carNumber = commands[1];
+                if (direction == "IN")
+                {
+                    parkingLot.Add(carNumber);
+                }
+                else
+                {
+                    parkingLot.Remove(carNumber);
+                }               
+            }
+            if (parkingLot.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, parkingLot));
+            }
+            
+        }
+    }
+}
