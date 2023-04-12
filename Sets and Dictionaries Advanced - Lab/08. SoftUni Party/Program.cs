80f4e12cf095f08c069a9fa0b28473a3ff240e86
MustyX23 <mustyx900@gmail.com>
2023-04-12 21:14:08 +0300

    08. SoftUni Party\Program.cs

---
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new HashSet<string>();
+
+            string input;
+
+            while ((input = Console.ReadLine())!= "PARTY")
+            {
+                string reservationNums = input;
+                guests.Add(reservationNums);
+            }
+
+            while((input = Console.ReadLine()) != "END")
+            {
+                string people = input;
+
+                if (guests.Contains(people))
+                {
+                    guests.Remove(people);
+                }
+            }
+
+            Console.WriteLine(guests.Count);
+
+            foreach (var guest in guests)
+            {
+                if (Char.IsDigit(guest[0]))
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+
+            foreach (var guest in guests)
+            {
+                if (Char.IsLetter(guest[0]))
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+        }
+    }
+}
