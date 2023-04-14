a0ebc2a3af7c49bd8792875db03d0d08f6d43291
MustyX23 <mustyx900@gmail.com>
2023-04-15 00:02:39 +0300

    01. Action Print\Program.cs

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]stringCollection = Console.ReadLine().Split(' ');
+
+            Action<string> print = Console.WriteLine;
+
+            PrintNames(stringCollection, print);
+        }
+
+        static void PrintNames(string[] collection, Action<string> printAction)
+        {
+            foreach (string item in collection)
+            {
+                printAction(item);
+            }                
+        }
+    }
+}
