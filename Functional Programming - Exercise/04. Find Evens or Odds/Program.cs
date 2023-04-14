6004b84083d6cb4204d32f0039186def280d2400
MustyX23 <mustyx900@gmail.com>
2023-04-15 00:03:16 +0300

    04. Find Evens or Odds\Program.cs

---
+++ Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int start = nums[0];
+            int end = nums[1];
+
+            string action = Console.ReadLine();
+            Predicate<int> filter;
+            Action<string> print = Console.Write;
+
+            if (action == "even")
+            {
+                filter = x => x % 2 == 0;
+            }
+            else
+            {
+                filter = x => x % 2 != 0;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                if (filter(i))
+                {
+                    print(i + " ");
+                }
+            }
+
+        }
+    }
+}
