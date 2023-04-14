9dae220599b56488de9aebb65e504d7a8f07d529
MustyX23 <mustyx900@gmail.com>
2023-04-15 00:02:57 +0300

    02. Knights of Honor\Program.cs

---
+++ Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ");
+            Action<string> print = Console.WriteLine;
+
+            PrintNamesWithPrefix(names, "Sir", print);
+        }
+
+        static void PrintNamesWithPrefix(string[]names, string prefix, Action<string> print)
+        {
+            foreach (var name in names)
+            {
+                print($"{prefix} {name}");
+            }             
+        }
+    }
+}
