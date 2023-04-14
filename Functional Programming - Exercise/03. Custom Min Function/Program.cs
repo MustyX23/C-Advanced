397ff8cce0811bf8ec864ca39ad3d8c2cc899ca4
MustyX23 <mustyx900@gmail.com>
2023-04-15 00:03:07 +0300

    03. Custom Min Function\Program.cs

---
+++ Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Func<int[], int> findSmallestNum = n => n.Min();
+
+            int smallestNum = findSmallestNum(nums);
+
+            Console.WriteLine(smallestNum);
+        }
+    }
+}
