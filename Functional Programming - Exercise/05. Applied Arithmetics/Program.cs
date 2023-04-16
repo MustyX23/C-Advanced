8139514e9160cfbe6a44d6192149eaad4dd89479
MustyX23 <mustyx900@gmail.com>
2023-04-17 00:53:02 +0300

    05. Applied Arithmetics\Program.cs

---
+++ Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();           
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "add")
+                {
+                    nums = nums.Select(n => n + 1).ToList();
+                }
+                else if (input == "multiply")
+                {
+                    nums = nums.Select(n => n * 2).ToList();
+                }
+                else if (input == "subtract")
+                {
+                    nums = nums.Select(n => n - 1).ToList();
+                }
+                else if (input == "print")
+                {
+                    Console.WriteLine(string.Join(' ', nums));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
