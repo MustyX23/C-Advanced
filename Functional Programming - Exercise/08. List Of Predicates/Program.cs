2628bf3351aebab55da3b74640ba559b8d025151
MustyX23 <mustyx900@gmail.com>
2023-04-17 00:53:25 +0300

    08. List Of Predicates\Program.cs

---
+++ Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[]array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        }
+    }
+}
