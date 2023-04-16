0cfaa08f4f022134dd4229be5006c40fccec375b
MustyX23 <mustyx900@gmail.com>
2023-04-17 00:53:17 +0300

    07. Predicate For Names\Program.cs

---
+++ Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ")
+                .Where(name => name.Length <= n)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+
+        }
+    }
+}
