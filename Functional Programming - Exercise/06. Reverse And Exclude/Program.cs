2680af47ae30028edae2163449180f66f7215112
MustyX23 <mustyx900@gmail.com>
2023-04-17 00:53:10 +0300

    06. Reverse And Exclude\Program.cs

---
+++ Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            nums.Reverse();
+            int divisible = int.Parse(Console.ReadLine());
+
+            nums = nums.Where(x => x % divisible != 0).ToList();
+
+            Console.WriteLine(String.Join(' ', nums));
+        }
+    }
+}
