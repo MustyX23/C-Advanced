8ae92dd23eb77b596084f2da82caae5d89adb278
MustyX23 <mustyx900@gmail.com>
2023-04-17 21:34:31 +0300

    01. Car\Program.cs

---
+++ Defining Classes - Lab/01. Car/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Mark = "BMW",
+                Model = "3",
+                Year = 2018
+            };
+
+            Console.WriteLine(car.Mark);
+        }
+    }
+}
