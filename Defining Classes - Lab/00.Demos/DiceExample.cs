9e20c6b7733a0339bd66df796373d5fbd72ba5c5
MustyX23 <mustyx900@gmail.com>
2023-04-17 21:34:09 +0300

    DiceExample.cs

---
+++ Defining Classes - Lab/00.Demos/DiceExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.Demos
+{
+    class Dice
+    {
+        public int Side { get; set; }
+        public string Color { get; set; }
+    }
+}
