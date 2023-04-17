92810f0369e2ffce18424bcc79b34b3c87adf16d
MustyX23 <mustyx900@gmail.com>
2023-04-17 21:34:54 +0300

    02. Car Extension\StartUp.cs

---
+++ Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "VW",
+                Model = "MK3",
+                Year = 1992,
+                FuelConsumption = 0.3,
+                FuelQuantity = 100,                                
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Where to go?");
+
+                car.Drive(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine($"Fuel left: {car.FuelQuantity}");
+            }
+            
+        }
+    }
+}
