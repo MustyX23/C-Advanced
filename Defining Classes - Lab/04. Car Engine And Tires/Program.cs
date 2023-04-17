5a050e71f2cb4a65789675f5ffcee6cca16c3997
MustyX23 <mustyx900@gmail.com>
2023-04-18 01:50:55 +0300

    04. Car Engine And Tires\Program.cs

---
+++ Defining Classes - Lab/04. Car Engine And Tires/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+            Tire[] tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.6),
+                new Tire(2, 1.3),
+            };
+
+            Engine engine = new Engine(560, 6000);
+
+            Car car = new Car("Lambo", "Urus", 2021, 250, 9, engine, tires);
+
+        }
+    }
+}
