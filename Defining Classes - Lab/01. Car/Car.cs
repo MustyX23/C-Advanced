7e8707f95a62bc2a14c4da30f568a5ad11117632
MustyX23 <mustyx900@gmail.com>
2023-04-17 21:34:28 +0300

    01. Car\Car.cs

---
+++ Defining Classes - Lab/01. Car/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string mark;
+        private string model;
+        private int year;
+
+        public string Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }           
+        }
+    }
+}
