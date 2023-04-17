2a15c6c0fd50a84b570cc983c97acc55113455f0
MustyX23 <mustyx900@gmail.com>
2023-04-18 02:22:57 +0300

    Update of my classes and objects knowledge

---
+++ Defining Classes - Lab/Practise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Mustafa",
+                Age = 13,
+                Gender = "male"
+            };
+            person.Introduce();
+        }
+    }
+    class Person 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Person Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {Name}. I'm {Age} years old and I identify as {Gender}.");
+            return new Person();
+        }
+    }
+}
