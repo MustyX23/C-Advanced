5857e02f4849a971543c74930efc419fc74ba16e
MustyX23 <mustyx900@gmail.com>
2023-04-20 01:10:35 +0300

    DefiningClasses\Program.cs

---
+++ Defining Classes - Exercise/DefiningClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Hasan", 13);
+
+            Console.WriteLine(person.Name +" - " +person.Age);
+        }
+    }
+}
