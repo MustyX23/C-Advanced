95c998a49b68dc15586810e1c66ace245de9a74b
MustyX23 <mustyx900@gmail.com>
2023-04-20 01:10:47 +0300

    StartUp\Program.cs

---
+++ Defining Classes - Exercise/StartUp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            
+            for (int i = 0; i < 3; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                Person person = new Person();
+                //Name	Age
+                //Peter - 20
+                //George - 18
+                //Jose - 43
+                person.Name = name;
+                person.Age = age;
+
+                people.Add(person);
+            }
+
+            //Use the type of people which is Person to cycle through the List<Person> people
+            foreach (Person person in people)
+            {
+                //Print person's name and age...
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+        }
+    }    
+}
