6c61e241925551bc043e4ab3ea2f1b3a209a4b8d
MustyX23 <mustyx900@gmail.com>
2023-04-20 01:11:37 +0300

    VideoGame Hierarchy\Program.cs

---
+++ Defining Classes - Lab/VideoGame Hierarchy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VideoGame_Hierarchy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class GameObject
+    {
+
+    }
+    public class PlayerCharacter
+    {
+        //health, armor, damage, and position (x and y coordinates)
+        
+    }
+    public class EnemyCharacter
+    {
+
+    }
+}
