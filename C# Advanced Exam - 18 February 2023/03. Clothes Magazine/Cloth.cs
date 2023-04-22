56ad6fe5c3dc8eaf64315203f8b972a9d939bf9f
MustyX23 <mustyx900@gmail.com>
2023-04-22 05:53:13 +0300

    03. Clothes Magazine\Cloth.cs

---
+++ C# Advanced Exam - 18 February 2023/03. Clothes Magazine/Cloth.cs
@@ -0,0 +1,24 @@
+﻿namespace ClothesMagazine
+{
+    public class Cloth
+    {
+        public Cloth(string color, int size, string type)
+        {
+            Color = color;
+            Size = size;
+            Type = type;
+        }
+
+        //•	Color - string
+        public string Color { get; set; }
+        //•	Size - int
+        public int Size { get; set; }
+        //•	Type - string
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: {Type} with size {Size}, color {Color}";
+        }
+    }
+}
