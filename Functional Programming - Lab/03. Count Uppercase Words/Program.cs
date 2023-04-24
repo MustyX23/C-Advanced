4a79937c435ddba96dcb2e5bc173bef84f596db2
MustyX23 <mustyx900@gmail.com>
2023-04-24 21:28:07 +0300

    03. Count Uppercase Words\Program.cs

---
+++ Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = x => char.IsUpper(x[0]);
+            var words = Console.ReadLine().Split(" ").Where(checker);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
