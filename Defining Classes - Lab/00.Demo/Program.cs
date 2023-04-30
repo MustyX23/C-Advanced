9c1a6e08ab55cca60d3ce20a07b5b2db23117fc7
MustyX23 <mustyx900@gmail.com>
2023-04-30 22:47:51 +0300

    00.Demo\Program.cs

---
+++ Defining Classes - Lab/00.Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _00.Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Console.WriteLine(dog.Name = "Sharo");
+            dog.AnimalSound();
+            Cat cat = new Cat();
+            Console.WriteLine(cat.Name = "Macka");
+            cat.AnimalSound();
+        }
+    }
+    public class Animal
+    {
+        public void AnimalSound()
+        {
+            Console.WriteLine("An animal makes this sound:");
+        }
+    }
+    public class Cat : Animal
+    {
+        public string Name { get; set; }
+        public void AnimalSound()
+        {
+
+            Console.WriteLine("Meow, Meow, Meeooow");
+        }
+    }
+    public class Dog : Animal
+    {
+        public string Name { get; set; }
+        public void AnimalSound()
+        {
+            Console.WriteLine("Woof, Wooof, Wooooff");
+        }
+    }
+
+}
