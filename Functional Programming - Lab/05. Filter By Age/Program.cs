2e295998994dc21b7610cb9004b8fb2dd4d95352
MustyX23 <mustyx900@gmail.com>
2023-04-30 22:48:13 +0300

    05. Filter By Age\Program.cs

---
+++ Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Filter_By_Age
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            Func<Person, bool> filter = p => true;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ");
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+               
+                Person person = new Person();
+                person.Age = age;
+                person.Name = name;
+                people.Add(person);
+            }
+
+            string command = Console.ReadLine();
+            int agePattern = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine(); // name/age/name age
+
+            if (command == "older")
+            {
+                filter = p => p.Age >= agePattern;
+            }
+            else
+            {
+                filter = p => p.Age < agePattern;
+            }
+
+            var peopleFilter = people.Where(filter);
+
+            Func<Person, string> print = p => p.Name + " " + p.Age;
+            List<Person> filterPeople = people.Where(filter).ToList();
+            if (format == "name")
+            {
+                print = p => p.Name;
+            }
+            else if (format == "age")
+            {
+                print = p => p.Age.ToString();
+            }
+            else
+            {
+                print = p => p.Name + " - " + p.Age;
+            }
+
+            List<string> printPeople = filterPeople.Select(print).ToList();
+            
+            foreach (var item in printPeople)
+            {
+                Console.WriteLine(item);
+            }
+        }        
+    }
+}
