b063d533f178b326725fb482d2e339cb572b000e
MustyX23 <mustyx900@gmail.com>
2023-04-30 22:48:02 +0300

    04. Add VAT\Program.cs

---
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .ToArray();
+
+            Func<double, double> vat = x => x * 0.2 + x;
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{vat(number):f2}");
+            }
+        }
+    }
+}
