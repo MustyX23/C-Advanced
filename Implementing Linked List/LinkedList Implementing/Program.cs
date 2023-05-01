1e6026e003aeefefd785eb49044b5ad23d620a8f
MustyX23 <mustyx900@gmail.com>
2023-05-01 10:38:55 +0300

    Creating LinkedList

---
+++ Implementing Linked List/LinkedList Implementing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList_Implementing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a LinkedList 
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            
+
+        }
+    }
+}
