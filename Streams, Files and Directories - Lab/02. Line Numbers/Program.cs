d2be45297e5efcdf3330ac98abd2b85ba0cbb45e
MustyX23 <mustyx900@gmail.com>
2023-05-03 23:02:34 +0300

    02. Line Numbers\Program.cs

---
+++ Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"../../../input.txt";
+            string outputPath = @"../../../output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                int counter = 1;
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter++}. {line}");
+                        line = reader.ReadLine();
+                    }
+                }
+                
+            }
+        }
+    }
+}
