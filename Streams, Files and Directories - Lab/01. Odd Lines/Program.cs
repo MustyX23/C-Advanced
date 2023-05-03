367cf7c6bb56c1ca63eb4bb6e37ca89b7104804c
MustyX23 <mustyx900@gmail.com>
2023-05-03 23:02:14 +0300

    01. Odd Lines\Program.cs

---
+++ Streams, Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../../input.txt");
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
