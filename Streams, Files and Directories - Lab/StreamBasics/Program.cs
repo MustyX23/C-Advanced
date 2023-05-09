f7324c77df251fad44e96d25ca6b07c11c1f0579
MustyX23 <mustyx900@gmail.com>
2023-05-09 23:51:15 +0300

    StreamBasics\Program.cs

---
+++ Streams, Files and Directories - Lab/StreamBasics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace StreamBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using StreamReader
+            StreamReader reader = new StreamReader("../../../input.txt");
+
+            using (reader)
+            {
+                int counter = 1;
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {                   
+                    Console.WriteLine($"{counter++}. {line}");
+                    line = reader.ReadLine();
+                }
+            }
+
+            //Using StreamWriter
+            StreamWriter writer = new StreamWriter("../../../input.txt");
+            using (writer)
+            {
+                //It is re-writing the old input.txt
+                writer.WriteLine("I am writing in the StreamWriter");
+            }
+        }
+    }
+}
