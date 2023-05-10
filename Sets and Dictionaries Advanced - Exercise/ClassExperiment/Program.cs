f76e4a87db4bb1a9be70d2e3ead5ee08afa68001
MustyX23 <mustyx900@gmail.com>
2023-05-10 15:37:31 +0300

    ClassExperiment\Program.cs

---
+++ Sets and Dictionaries Advanced - Exercise/ClassExperiment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassExperiment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
