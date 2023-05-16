59f590d8a245993f098db121f33d6a64cd57d3a9
MustyX23 <mustyx900@gmail.com>
2023-05-16 23:31:21 +0300

    Generics\Program.cs

---
+++ Generics - Lab/Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generics introduce the concept of Type Parameters
+
+            //Allow designing classes and methods without parameter type specification
+
+            //Powerful to Reuse Code
+            CustomStack<int> customStack = new CustomStack<int>();
+        }
+    }
+
+    //Initializing the Generic class
+    public class CustomStack<T>
+    {
+
+    }
+    //Blueprint for a type - T (Type Parameter)
+    public class List<T>
+    {
+        //public T Add (T element) {...}
+        //public T Remove(T element) {...};
+        //public T Peek(T element){...};
+    }
+}
