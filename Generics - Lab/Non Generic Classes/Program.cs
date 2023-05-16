391efda59a0e80c7a1e472f725332922296396a3
MustyX23 <mustyx900@gmail.com>
2023-05-16 23:31:31 +0300

    Non Generic Classes\Program.cs

---
+++ Generics - Lab/Non Generic Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Non_Generic_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementing Non Generic Classes
+            object[] array = new object[] {1, 2 ,3 ,4};
+            ObjectList nonGenericList = new ObjectList(array);
+        }
+    }
+    public class ObjectList 
+    {
+        private object[] elements;
+
+        public ObjectList(object[] elements)
+        {
+            this.elements = new object[4];
+        }
+
+        public void Add(object element)
+        {
+            //...
+        }
+        public object GetIndex(int index)
+        {
+            return this.elements[index];
+        }
+    }
+}
