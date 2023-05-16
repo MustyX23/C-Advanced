98c0310a3fee1731166c360af6fcc7b3fcd5534d
MustyX23 <mustyx900@gmail.com>
2023-05-16 23:31:08 +0300

    GenericClasses\Program.cs

---
+++ Generics - Lab/GenericClasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Encapsulate operations to a non-particular data type
+            //Defined with Type Parameters - T
+
+            //Most commonly used are generic collections:
+            //Linked Lists, Hash tables, Stacks, Queues, Trees, etc
+            List<string> listOfStrings = new List<string>() {"Fin", "Jake"};
+
+            List<Student> listOfStudents = new List<Student>();
+            listOfStudents.Add(new Student("Fin", 13));
+
+            Stack<int> stacks = new Stack<int>();
+            HashSet<int> set = new HashSet<int>();
+
+            //Collections with multiple type parameters – Dictionary<T, V>
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+        }
+    }
+    public class Student 
+    {
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
