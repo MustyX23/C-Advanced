7a43fd4fa7c3d5d72998b5dd8f7ca9af48525dd1
MustyX23 <mustyx900@gmail.com>
2023-05-27 23:15:19 +0300

    02. Blind Man's Buff\Program.cs

--- C# Advanced Exam - 18 February 2023/02. Blind Man's Buff/Program.cs
+++ C# Advanced Exam - 18 February 2023/02. Blind Man's Buff/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02._Blind_Man_s_Buff
 {
@@ -6,7 +7,100 @@ namespace _02._Blind_Man_s_Buff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            string[,] playground = new string[dimensions[0], dimensions[1]];
+
+            int myRow = -1;
+            int myCol = -1;
+            int opponentsTouched = 0;
+            int moves = 0;
+
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                string[] row = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < dimensions[1]; j++)
+                {
+                    playground[i, j] = row[j].ToString();
+                    if (playground[i, j] == "B")
+                    {
+                        myRow = i;
+                        myCol = j;
+                        playground[i, j] = "-";
+                    }
+                }
+            }
+
+            string direction;
+            while ((direction = Console.ReadLine()) != "Finish" || opponentsTouched != 3)
+            {
+                if ((direction == "left" && myCol == 0) ||
+                    (direction == "right" && myCol == dimensions[1] - 1) ||
+                    (direction == "up" && myRow == 0) ||
+                    (direction == "down" && myRow == dimensions[0] - 1))
+                {
+                    continue;
+                }
+
+                switch (direction)
+                {
+                    case "left":
+                        if (playground[myRow, myCol - 1] == "O")
+                        {
+                            continue;
+                        }
+                        break;
+                    case "right":
+                        if (playground[myRow, myCol + 1] == "O")
+                        {
+                            continue;
+                        }
+                        break;
+                    case "up":
+                        if (playground[myRow - 1, myCol] == "O")
+                        {
+                            continue;
+                        }
+                        break;
+                    case "down":
+                        if (playground[myRow + 1, myCol] == "O")
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                moves++;
+                switch (direction)
+                {
+                    case "left":
+                        myCol--;
+                        break;
+                    case "right":
+                        myCol++;
+                        break;
+                    case "up":
+                        myRow--;
+                        break;
+                    case "down":
+                        myRow++;
+                        break;
+                }
+
+                if (playground[myRow, myCol] == "P")
+                {
+                    opponentsTouched++;
+                    playground[myRow, myCol] = "-";
+
+                    if (opponentsTouched == 3)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Game over!");
+            Console.WriteLine($"Touched opponents: {opponentsTouched} Moves made: {moves}");
         }
     }
 }
