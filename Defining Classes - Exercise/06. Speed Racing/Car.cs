bdcdc46a2c0140e9a9d9deacce219f66da3ee37e
MustyX23 <mustyx900@gmail.com>
2023-05-30 00:20:45 +0300

    06. Speed Racing\Car.cs

---
+++ Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = 0;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public bool CanMove(double distance)
+        {
+            double fuelNeeded = distance * FuelConsumptionPerKilometer;
+            if (FuelAmount >= fuelNeeded)
+            {
+                FuelAmount -= fuelNeeded;
+                TravelledDistance += distance;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:f2} {TravelledDistance}";
+        }
+    }
+}
