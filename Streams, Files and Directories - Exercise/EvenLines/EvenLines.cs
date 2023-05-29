f9e583c296cd2cd8bf29e16a99a5413bba83e78d
MustyX23 <mustyx900@gmail.com>
2023-05-29 14:20:11 +0300

    EvenLines\EvenLines.cs

---
+++ Streams, Files and Directories - Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,47 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            StreamReader streamReader = new StreamReader(inputFilePath);
+            Console.WriteLine(ProcessLines(inputFilePath));
+
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder result = new StringBuilder();
+            using (StreamReader streamReader = new StreamReader(inputFilePath))
+            {
+                int countOfLines = 0;
+
+                while (!streamReader.EndOfStream)
+                {
+                    
+                    string line = streamReader.ReadLine();
+                    if (countOfLines % 2 == 0)
+                    {
+                        line = line.Replace("-", "@")
+                            .Replace(",", "@")
+                            .Replace(".", "@")
+                            .Replace("!", "@")
+                            .Replace("?", "@");
+                        string[]words = line.Split(' ');
+                        Array.Reverse(words);
+                        line = string.Join(' ',words);
+                        result.AppendLine(line);
+                    }
+                    countOfLines++;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
