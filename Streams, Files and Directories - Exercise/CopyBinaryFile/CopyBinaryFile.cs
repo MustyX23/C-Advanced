b44c64e88176f57e7fc0ecaf587393ad41c07649
MustyX23 <mustyx900@gmail.com>
2023-05-29 14:19:24 +0300

    CopyBinaryFile\CopyBinaryFile.cs

---
+++ Streams, Files and Directories - Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,35 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            const int bufferSize = 4096; // 4KB buffer size
+
+            using (FileStream source = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream destination = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] buffer = new byte[bufferSize];
+                    int bytesRead;
+
+                    while ((bytesRead = source.Read(buffer, 0, bufferSize)) > 0)
+                    {
+                        destination.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
