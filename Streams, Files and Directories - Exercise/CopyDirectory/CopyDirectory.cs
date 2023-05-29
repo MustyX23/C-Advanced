be074d4ea569ad6ef97edf848623a0181890f99a
MustyX23 <mustyx900@gmail.com>
2023-05-29 14:19:47 +0300

    CopyDirectory\CopyDirectory.cs

---
+++ Streams, Files and Directories - Exercise/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,37 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath =  @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string destinationPath = Path.Combine(outputPath, fileName);
+                File.Copy(file, destinationPath);
+            }
+
+            Console.WriteLine("Directory copied successfully!");
+        }
+    }
+}
