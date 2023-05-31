9cfa71d4c4c3511172843d27711cf8ef53d245da
MustyX23 <mustyx900@gmail.com>
2023-05-31 14:26:46 +0300

    02. The Squirrel\Program.cs

---
+++ C# Advanced Retake Exam - 12 April 2023/02. The Squirrel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._The_Squirrel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            Solution solution = new Solution();
+            Console.WriteLine(string.Join(' ', solution.TwoSum(nums, 9)));
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            //Input: nums = [2,7,11,15], target = 9
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length - 1; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            //Output: [0,1]
+            //Explanation: Because nums[0] +nums[1] == 9, we return [0, 1].
+            throw new Exception("Not found.");
+        }
+    }
+}
