cc272efdeac4f664e79d7fd004772252e8016c14
MustyX23 <mustyx900@gmail.com>
2023-06-01 20:55:12 +0300

    09. Pokemon Trainer\Trainer.cs

---
+++ Defining Classes - Exercise/09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Collection { get; set; }
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Collection = new List<Pokemon>();
+        }
+
+        public bool HasPokemon(string element) 
+        {
+            return Collection.Any(pokemon => pokemon.Element == element);
+        }
+        public int IncreaseBadges()
+        {
+            return Badges++;
+        }
+        public void DecreaseHealth() 
+        {
+            foreach (var pokemon in Collection)
+            {
+                pokemon.Health -= 10;
+            }
+        }
+        public void RemoveDeathPokemons()
+        {
+            Collection.RemoveAll(pokemon => pokemon.Health <= 0);
+        }
+                
+    }   
+}
