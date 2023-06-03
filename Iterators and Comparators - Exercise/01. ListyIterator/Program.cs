f552bcd643f6398eec19881d78f5ece9ceee3c2e
MustyX23 <mustyx900@gmail.com>
2023-06-03 06:56:02 +0300

    01. ListyIterator\Program.cs

---
+++ Iterators and Comparators - Exercise/01. ListyIterator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            ListyIterator<string> iterator = new ListyIterator<string>();
+
+            string command;
+
+            while ((command = Console.ReadLine())!= "END")
+            {
+                try
+                {
+                    if (command == "Move")
+                    {
+                        iterator.Move();
+                    }
+                    else if (command == "HasNext")
+                    {
+                        iterator.HasNext();
+                    }
+                    else if (command == "Print")
+                    {
+                        iterator.Print();
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
