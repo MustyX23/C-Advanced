4434ab30f16cfe9b3ff5774d34c479e2ab3f5009
MustyX23 <mustyx900@gmail.com>
2023-06-05 23:47:45 +0300

    07. Custom Comparator\Program.cs

---
+++ Iterators and Comparators - Exercise/07. Custom Comparator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Custom_Comparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> customComparator = (x, y) =>
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+                if (x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+                return x.CompareTo(y);
+            };
+
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (x,y) => customComparator(x, y));
+
+            Console.WriteLine(String.Join(' ', numbers));
+            
+        }
+    }
+}
