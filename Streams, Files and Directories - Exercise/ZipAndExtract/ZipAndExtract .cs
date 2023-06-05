40ef21935b59f28bde8610c5113f3d90f67e6af8
MustyX23 <mustyx900@gmail.com>
2023-06-05 23:48:28 +0300

    ZipAndExtract .cs

---
+++ Streams, Files and Directories - Exercise/ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,41 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (var zipArchive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create))
+            {
+                zipArchive.CreateEntryFromFile(inputFilePath, Path.GetFileName(inputFilePath));
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using (var zipArchive = ZipFile.OpenRead(zipArchiveFilePath))
+            {
+                var entry = zipArchive.GetEntry(fileName);
+                if (entry != null)
+                {
+                    entry.ExtractToFile(outputFilePath);
+                }
+            }
+        }
+    }
+}
